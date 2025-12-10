27225ab02ccca6658b7911fc59e8b2a7d26666e5
tragic-cmd <codyshouey@gmail.com>
2025-12-09 20:09:35 -0500

    B: Edited gitignore, added --verbose option to prompt, added function to get file information for additional features

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
 import argparse
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.prompt`
 
     prompt = args.prompt
     load_dotenv()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
     # API
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,7 +24,7 @@ def main():
     # Gemini
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=prompt
+        model='gemini-2.5-flash', contents=messages
     )
 
     # Response
@@ -28,10 +32,13 @@ def main():
         raise RuntimeError("Failed API request.")
     prompt_token_count = response.usage_metadata.prompt_token_count
     candidates_token_count = response.usage_metadata.candidates_token_count
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {candidates_token_count}")
-    print(f"Response: {response.text}")
+    if args.verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {prompt_token_count}")
+        print(f"Response tokens: {candidates_token_count}")
+        print(f"Response: {response.text}")
+    else:
+        print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
